96a376a8f4047f91c4b530fabcba7bd013d49730
Alexandre Peyroux <alex@xn--wxa.email>
2017-07-28 17:34:38 +0200

    draft add to ipfs

--- src/Network/IPFS.hs
+++ src/Network/IPFS.hs
@@ -4,10 +4,15 @@
 
 module Network.IPFS where
 
-import Network.Wreq
-import Text.Printf
 import           Control.Arrow ((>>>), (&&&))
 import           Control.Lens ((^.), (^?), (^..), ix, (.~), (&))
+import           Data.Aeson
+import           Data.Aeson.Lens (key, _Array, _String)
+import           Data.Map
+import qualified Data.Text as T
+import           GHC.Generics (Generic)
+import           Network.Wreq
+import           Text.Printf
 
 newtype IPFS = IPFS {
   ipfsEndPoint :: String
@@ -18,3 +23,10 @@ ipfsDefaultConfig = IPFS "http://127.0.0.1:5001/api/v0/"
 
 ipfsPing :: IO Int
 ipfsPing = get (printf "%s/ping" (ipfsEndPoint ipfsDefaultConfig)) >>= ((^. responseStatus . statusCode) >>> return)
+
+ipfsAdd :: IO T.Text
+ipfsAdd = do
+  q <- postWith opts (printf "%s/add" (ipfsEndPoint ipfsDefaultConfig)) (partFile "file" "/var/www/alex/index.html")
+  return $ q ^. responseBody . key "Hash" . _String
+  where
+    opts = defaults & param "recursive" .~ ["True"]
